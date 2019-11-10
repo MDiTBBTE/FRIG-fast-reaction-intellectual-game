2962a21716ff27fe2cf5f1cced581aa58b831baf
mditbbte <andrii.ds.kor@gmail.com>
2019-11-10 10:36:55 +0200

    Updates

---
+++ precache-manifest.da19cea2b74b7ad4af2b4e24fe8a33eb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "655b493ed73ebc7e0c3471088522a0d2",
+    "url": "/STARNAVI-test/index.html"
+  },
+  {
+    "revision": "8016f22e99ff3e77d401",
+    "url": "/STARNAVI-test/static/css/4.774b713a.chunk.css"
+  },
+  {
+    "revision": "48981618a3a424549d11",
+    "url": "/STARNAVI-test/static/css/5.92388181.chunk.css"
+  },
+  {
+    "revision": "f06c613ff70ccfd6a11a",
+    "url": "/STARNAVI-test/static/css/main.5aea4f62.chunk.css"
+  },
+  {
+    "revision": "6e92d613fe9f22960083",
+    "url": "/STARNAVI-test/static/js/2.2d56e337.chunk.js"
+  },
+  {
+    "revision": "707b6f82ab68d7fd41d2",
+    "url": "/STARNAVI-test/static/js/3.8f9d8568.chunk.js"
+  },
+  {
+    "revision": "8016f22e99ff3e77d401",
+    "url": "/STARNAVI-test/static/js/4.c3b06790.chunk.js"
+  },
+  {
+    "revision": "48981618a3a424549d11",
+    "url": "/STARNAVI-test/static/js/5.8e605257.chunk.js"
+  },
+  {
+    "revision": "f06c613ff70ccfd6a11a",
+    "url": "/STARNAVI-test/static/js/main.ede1c567.chunk.js"
+  },
+  {
+    "revision": "e0f2c3c19702b62357e3",
+    "url": "/STARNAVI-test/static/js/runtime-main.3140600c.js"
+  }
+]);
